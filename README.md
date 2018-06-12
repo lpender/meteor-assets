32bdd81f8d9ea0f9deea5d123b3dcac3a86ebe52
RJ David <rj@rjdavid.ph>
2018-06-12 19:19:23 +0800

    Updated to include new icons / launchscreens and remove deprecated ones

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Setup
 
-- Clone this repository.
-- Open the repository in the command line and run `npm install`
+-   Clone this repository.
+-   Open the repository in the command line and run `npm install`
 
 ```
 git clone https://github.com/lpender/meteor-assets
@@ -42,81 +42,88 @@ git checkout v0.0.2
 
 ## Usage
 
-*Note*: You can use the same icon cross platform but it's not recommended in
+_Note_: You can use the same icon cross platform but it's not recommended in
 some cases. See [this issue](https://github.com/lpender/meteor-assets/issues/6)
 for more details.
 
-1. Generate icons at 1024x1024 and place them in `resources/icon-ios.png` and
-   `resources/icon-android.png`.
+1.  Generate icons at 1024x1024 and place them in `resources/icon-ios.png` and
+    `resources/icon-android.png`.
 
-2. Generate splash screens at 2208x2208 and place them in
-   `resources/splash-ios.png` and `resources/splash-android.png`.
+2.  Generate splash screens at 2208x2208 and place them in
+    `resources/splash-ios.png` and `resources/splash-android.png`.
 
-3. Run `node meteor-assets`.
+3.  Run `node meteor-assets`.
 
-4. Copy the `resources` directory to your app: `cp -R resources /path/to/my/app`.
+4.  Copy the `resources` directory to your app: `cp -R resources /path/to/my/app`.
 
-5. Add this to your `mobile-config.js`
+5.  Add this to your `mobile-config.js`
 
-``` javascript
+```javascript
 App.icons({
-  "app_store": "resources/icons/app_store.png", // 1024x1024
-  "iphone_2x": "resources/icons/iphone_2x.png", // 120x120
-  "iphone_3x": "resources/icons/iphone_3x.png", // 180x180
-  "ipad": "resources/icons/ipad.png", // 76x76
-  "ipad_2x": "resources/icons/ipad_2x.png", // 152x152
-  "ipad_pro": "resources/icons/ipad_pro.png", // 167x167
-  "ios_settings": "resources/icons/ios_settings.png", // 29x29
-  "ios_settings_2x": "resources/icons/ios_settings_2x.png", // 58x58
-  "ios_settings_3x": "resources/icons/ios_settings_3x.png", // 87x87
-  "ios_spotlight": "resources/icons/ios_spotlight.png", // 40x40
-  "ios_spotlight_2x": "resources/icons/ios_spotlight_2x.png", // 80x80
-  "ios_notification": "resources/icons/ios_notification.png", // 20x20
-  "ios_notification_2x": "resources/icons/ios_notification_2x.png", // 40x40
-  "ios_notification_3x":"resources/icons/ios_notification_3x.png", // 60x60
-  "iphone_legacy": "resources/icons/iphone_legacy.png", // 57x57
-  "iphone_legacy_2x": "resources/icons/iphone_legacy_2x.png", // 114x114
-  "ipad_spotlight_legacy": "resources/icons/ipad_spotlight_legacy.png", // 50x50
-  "ipad_spotlight_legacy_2x": "resources/icons/ipad_spotlight_legacy_2x.png", // 100x100
-  "ipad_app_legacy": "resources/icons/ipad_app_legacy.png", // 72x72
-  "ipad_app_legacy_2x": "resources/icons/ipad_app_legacy_2x.png", // 144x144
-  "android_mdpi": "resources/icons/android_mdpi.png", // 48x48
-  "android_hdpi": "resources/icons/android_hdpi.png", // 72x72
-  "android_xhdpi": "resources/icons/android_xhdpi.png", // 96x96
-  "android_xxhdpi": "resources/icons/android_xxhdpi.png", // 144x144
-  "android_xxxhdpi": "resources/icons/android_xxxhdpi.png", // 192x192
-  "android_store": "resources/icons/android_store.png" // 512x512
+    app_store: 'resources/icons/app_store.png', // 1024x1024
+    iphone_2x: 'resources/icons/iphone_2x.png', // 120x120
+    iphone_3x: 'resources/icons/iphone_3x.png', // 180x180
+    ipad: 'resources/icons/ipad.png', // 76x76
+    ipad_2x: 'resources/icons/ipad_2x.png', // 152x152
+    ipad_pro: 'resources/icons/ipad_pro.png', // 167x167
+    ios_settings: 'resources/icons/ios_settings.png', // 29x29
+    ios_settings_2x: 'resources/icons/ios_settings_2x.png', // 58x58
+    ios_settings_3x: 'resources/icons/ios_settings_3x.png', // 87x87
+    ios_spotlight: 'resources/icons/ios_spotlight.png', // 40x40
+    ios_spotlight_2x: 'resources/icons/ios_spotlight_2x.png', // 80x80
+    ios_spotlight_3x: 'resources/icons/ios_spotlight_3x.png', // 120x120
+    ios_notification: 'resources/icons/ios_notification.png', // 20x20
+    ios_notification_2x: 'resources/icons/ios_notification_2x.png', // 40x40
+    ios_notification_3x: 'resources/icons/ios_notification_3x.png', // 60x60
+    iphone_legacy: 'resources/icons/iphone_legacy.png', // 57x57
+    iphone_legacy_2x: 'resources/icons/iphone_legacy_2x.png', // 114x114
+    ipad_spotlight_legacy: 'resources/icons/ipad_spotlight_legacy.png', // 50x50
+    ipad_spotlight_legacy_2x: 'resources/icons/ipad_spotlight_legacy_2x.png', // 100x100
+    ipad_app_legacy: 'resources/icons/ipad_app_legacy.png', // 72x72
+    ipad_app_legacy_2x: 'resources/icons/ipad_app_legacy_2x.png', // 144x144
+    android_mdpi: 'resources/icons/android_mdpi.png', // 48x48
+    android_hdpi: 'resources/icons/android_hdpi.png', // 72x72
+    android_xhdpi: 'resources/icons/android_xhdpi.png', // 96x96
+    android_xxhdpi: 'resources/icons/android_xxhdpi.png', // 144x144
+    android_xxxhdpi: 'resources/icons/android_xxxhdpi.png' // 192x192
 });
 
 App.launchScreens({
-  "iphone": "resources/splashes/iphone.png", // 320x480
-  "iphone_2x": "resources/splashes/iphone_2x.png", // 640x490
-  "iphone5": "resources/splashes/iphone5.png", // 640x1136
-  "iphone6": "resources/splashes/iphone6.png", // 750x1334
-  "iphone6p_portrait": "resources/splashes/iphone6p_portrait.png", // 2208x1242
-  "iphone6p_landscape": "resources/splashes/iphone6p_landscape.png", // 2208x1242
-  "ipad_portrait": "resources/splashes/ipad_portrait.png", // 768x1024
-  "ipad_portrait_2x": "resources/splashes/ipad_portrait_2x.png", // 1536x2048
-  "ipad_landscape": "resources/splashes/ipad_landscape.png", // 1024x768
-  "ipad_landscape_2x": "resources/splashes/ipad_landscape_2x.png", // 2048x1536
-  "android_mdpi_portrait": "resources/splashes/android_mdpi_portrait.png", // 320x480
-  "android_mdpi_landscape": "resources/splashes/android_mdpi_landscape.png", // 480x320
-  "android_hdpi_portrait": "resources/splashes/android_hdpi_portrait.png", // 480x800
-  "android_hdpi_landscape": "resources/splashes/android_hdpi_landscape.png", // 800x480
-  "android_xhdpi_portrait": "resources/splashes/android_xhdpi_portrait.png", // 720x1280
-  "android_xhdpi_landscape": "resources/splashes/android_xhdpi_landscape.png", // 1280x720
-  "android_xxhdpi_portrait": "resources/splashes/android_xxhdpi_portrait.png", // 1080x1440
-  "android_xxhdpi_landscape": "resources/splashes/android_xxhdpi_landscape.png", // 1440x1080
-  "android_featured": "resources/splashes/android_featured.png" // 1024x500
-})
+    iphone: 'resources/splashes/iphone.png', // 320x480
+    iphone_2x: 'resources/splashes/iphone_2x.png', // 640x490
+    iphone5: 'resources/splashes/iphone5.png', // 640x1136
+    iphone6: 'resources/splashes/iphone6.png', // 750x1334
+    iphone6p_portrait: 'resources/splashes/iphone6p_portrait.png', // 2208x1242
+    iphone6p_landscape: 'resources/splashes/iphone6p_landscape.png', // 2208x1242
+    iphoneX_portrait: 'resources/splashes/iphoneX_portrait.png', // 1125x2436
+    iphoneX_landscape: 'resources/splashes/iphoneX_landscape.png', // 2436x1125
+    ipad_portrait: 'resources/splashes/ipad_portrait.png', // 768x1024
+    ipad_portrait_2x: 'resources/splashes/ipad_portrait_2x.png', // 1536x2048
+    ipad_landscape: 'resources/splashes/ipad_landscape.png', // 1024x768
+    ipad_landscape_2x: 'resources/splashes/ipad_landscape_2x.png', // 2048x1536
+    android_mdpi_portrait: 'resources/splashes/android_mdpi_portrait.png', // 320x480
+    android_mdpi_landscape: 'resources/splashes/android_mdpi_landscape.png', // 480x320
+    android_hdpi_portrait: 'resources/splashes/android_hdpi_portrait.png', // 480x800
+    android_hdpi_landscape: 'resources/splashes/android_hdpi_landscape.png', // 800x480
+    android_xhdpi_portrait: 'resources/splashes/android_xhdpi_portrait.png', // 720x1280
+    android_xhdpi_landscape: 'resources/splashes/android_xhdpi_landscape.png', // 1280x720
+    android_xxhdpi_portrait: 'resources/splashes/android_xxhdpi_portrait.png', // 1080x1440
+    android_xxhdpi_landscape: 'resources/splashes/android_xxhdpi_landscape.png', // 1440x1080
+    android_xxxhdpi_portrait: 'resources/splashes/android_xxxhdpi_portrait.png', // 1280x1920
+    android_xxxhdpi_landscape:
+        'resources/splashes/android_xxxhdpi_landscape.png' // 1920x1280
+});
 ```
 
-Sizes thanks to https://github.com/meteor/meteor/blob/release-1.3/tools/cordova/builder.js
+Sizes thanks to
+
+-   https://docs.meteor.com/api/mobile-config.html#App-icons
+-   https://docs.meteor.com/api/mobile-config.html#App-launchScreens
 
 ## Notes
 
-- This will crop splashes horizontally centered and vertically centered.
-- This does not currently generate [9 patch](https://developer.android.com/guide/topics/graphics/2d-graphics.html#nine-patch) images for Android.
+-   This will crop splashes horizontally centered and vertically centered.
+-   This does not currently generate [9 patch](https://developer.android.com/guide/topics/graphics/2d-graphics.html#nine-patch) images for Android.
 
 ## Contributing
 
--- meteor-assets.js
+++ meteor-assets.js
@@ -1,140 +1,143 @@
 var fs = require('fs'),
-    gm = require('gm').subClass({imageMagick: true});
+    gm = require('gm').subClass({ imageMagick: true });
 
 var icons = [
-  {name:"app_store", size: "1024x1024"},
-  {name:"iphone_2x", size: "120x120"},
-  {name: "iphone_3x", size: "180x180"},
-  {name: "ipad", size: "76x76"},
-  {name: "ipad_2x", size: "152x152"},
-  {name: "ipad_pro", size: "167x167"},
-  {name: "ios_settings", size: "29x29"},
-  {name: "ios_settings_2x", size: "58x58"},
-  {name: "ios_settings_3x", size: "87x87"},
-  {name: "ios_spotlight", size: "40x40"},
-  {name: "ios_spotlight_2x", size: "80x80"},
-  {name: "ios_notification", size: "20x20"},
-  {name: "ios_notification_2x", size: "40x40"},
-  {name: "ios_notification_3x", size: "60x60"},
-  {name: "iphone_legacy", size: "57x57"},
-  {name: "iphone_legacy_2x", size: "114x114"},
-  {name: "ipad_spotlight_legacy", size: "50x50"},
-  {name: "ipad_spotlight_legacy_2x", size: "100x100"},
-  {name: "ipad_app_legacy", size: "72x72"},
-  {name: "ipad_app_legacy_2x", size: "144x144"},
-  {name: "android_mdpi", size: "48x48"},
-  {name: "android_hdpi", size: "72x72"},
-  {name: "android_xhdpi", size: "96x96"},
-  {name: "android_xxhdpi", size: "144x144"},
-  {name: "android_xxxhdpi", size: "192x192"},
-  {name: "android_store", size: "512x512"}
-]
+    { name: 'app_store', size: '1024x1024' },
+    { name: 'iphone_2x', size: '120x120' },
+    { name: 'iphone_3x', size: '180x180' },
+    { name: 'ipad', size: '76x76' },
+    { name: 'ipad_2x', size: '152x152' },
+    { name: 'ipad_pro', size: '167x167' },
+    { name: 'ios_settings', size: '29x29' },
+    { name: 'ios_settings_2x', size: '58x58' },
+    { name: 'ios_settings_3x', size: '87x87' },
+    { name: 'ios_spotlight', size: '40x40' },
+    { name: 'ios_spotlight_2x', size: '80x80' },
+    { name: 'ios_spotlight_3x', size: '120x120' },
+    { name: 'ios_notification', size: '20x20' },
+    { name: 'ios_notification_2x', size: '40x40' },
+    { name: 'ios_notification_3x', size: '60x60' },
+    { name: 'iphone_legacy', size: '57x57' },
+    { name: 'iphone_legacy_2x', size: '114x114' },
+    { name: 'ipad_spotlight_legacy', size: '50x50' },
+    { name: 'ipad_spotlight_legacy_2x', size: '100x100' },
+    { name: 'ipad_app_legacy', size: '72x72' },
+    { name: 'ipad_app_legacy_2x', size: '144x144' },
+    { name: 'android_mdpi', size: '48x48' },
+    { name: 'android_hdpi', size: '72x72' },
+    { name: 'android_xhdpi', size: '96x96' },
+    { name: 'android_xxhdpi', size: '144x144' },
+    { name: 'android_xxxhdpi', size: '192x192' }
+];
 
 var splashes = [
-  {"name":"iphone","size":"320x480"},
-  {"name":"iphone_2x","size":"640x960"},
-  {"name":"iphone5","size":"640x1136"},
-  {"name":"iphone6","size":"750x1334"},
-  {"name":"iphone6p_portrait","size":"1242x2208"},
-  {"name":"iphone6p_landscape","size":"2208x1242"},
-  {"name":"ipad_portrait","size":"768x1024"},
-  {"name":"ipad_portrait_2x","size":"1536x2048"},
-  {"name":"ipad_landscape","size":"1024x768"},
-  {"name":"ipad_landscape_2x","size":"2048x1536"},
-  {"name":"android_ldpi_landscape","size":"320x200"},
-  {"name":"android_mdpi_portrait","size":"320x480"},
-  {"name":"android_mdpi_landscape","size":"480x320"},
-  {"name":"android_hdpi_portrait","size":"480x800"},
-  {"name":"android_hdpi_landscape","size":"800x480"},
-  {"name":"android_xhdpi_portrait","size":"720x1280"},
-  {"name":"android_xhdpi_landscape","size":"1280x720"},
-  {"name":"android_xxhdpi_portrait","size":"1080x1440"},
-  {"name":"android_xxhdpi_landscape","size":"1440x1080"},
-  {"name":"android_featured","size":"1024x500"}
- ]
+    { name: 'iphone', size: '320x480' },
+    { name: 'iphone_2x', size: '640x960' },
+    { name: 'iphone5', size: '640x1136' },
+    { name: 'iphone6', size: '750x1334' },
+    { name: 'iphone6p_portrait', size: '1242x2208' },
+    { name: 'iphone6p_landscape', size: '2208x1242' },
+    { name: 'iphoneX_portrait', size: '1125x2436' },
+    { name: 'iphoneX_landscape', size: '2436x1125' },
+    { name: 'ipad_portrait', size: '768x1024' },
+    { name: 'ipad_portrait_2x', size: '1536x2048' },
+    { name: 'ipad_landscape', size: '1024x768' },
+    { name: 'ipad_landscape_2x', size: '2048x1536' },
+    { name: 'android_ldpi_landscape', size: '320x200' },
+    { name: 'android_mdpi_portrait', size: '320x480' },
+    { name: 'android_mdpi_landscape', size: '480x320' },
+    { name: 'android_hdpi_portrait', size: '480x800' },
+    { name: 'android_hdpi_landscape', size: '800x480' },
+    { name: 'android_xhdpi_portrait', size: '720x1280' },
+    { name: 'android_xhdpi_landscape', size: '1280x720' },
+    { name: 'android_xxhdpi_portrait', size: '1080x1440' },
+    { name: 'android_xxhdpi_landscape', size: '1440x1080' },
+    { name: 'android_xxxhdpi_portrait', size: '1280x1920' },
+    { name: 'android_xxxhdpi_landscape', size: '1920x1280' }
+];
 
 function getSize(image) {
-  var sizes = image.size.split('x');
-  return { width: sizes[0], height: sizes[1] }
+    var sizes = image.size.split('x');
+    return { width: sizes[0], height: sizes[1] };
 }
 
 // source file, target directory, image profile
 function resize(source, target, image) {
-  var icon = gm(source);
+    var icon = gm(source);
 
-  var imageSize = getSize(image);
-  var width = imageSize.width;
-  var height = imageSize.height;
-  var name = image.name;
+    var imageSize = getSize(image);
+    var width = imageSize.width;
+    var height = imageSize.height;
+    var name = image.name;
 
-  icon.resize(width, height)
-    .write(target + name + '.png', function(err) {
-      if(err) {
-        console.log(err);
-        console.log("Quitting because of an error");
-      } else {
-        console.log("Wrote " + name);
-      }
+    icon.resize(width, height).write(target + name + '.png', function(err) {
+        if (err) {
+            console.log(err);
+            console.log('Quitting because of an error');
+        } else {
+            console.log('Wrote ' + name);
+        }
     });
-};
+}
 
 function crop(source, target, image) {
-  var size;
-  var splash = gm(source);
+    var size;
+    var splash = gm(source);
 
-  var imageSize = getSize(image);
-  var width = parseInt(imageSize.width);
-  var height = parseInt(imageSize.height);
-  var name = image.name;
+    var imageSize = getSize(image);
+    var width = parseInt(imageSize.width);
+    var height = parseInt(imageSize.height);
+    var name = image.name;
 
-  // square up so that it cuts off as little as possible
-  if (height > width) {
-    splash.resize(height, height);
+    // square up so that it cuts off as little as possible
+    if (height > width) {
+        splash.resize(height, height);
 
-    size = height;
-  } else {
-    splash.resize(width, width);
+        size = height;
+    } else {
+        splash.resize(width, width);
 
-    size = width;
-  }
+        size = width;
+    }
 
-  // calculate x and y offset
-  var xOffset = (size/2) - (width/2);
-  var yOffset = (size/2) - (height/2);
+    // calculate x and y offset
+    var xOffset = size / 2 - width / 2;
+    var yOffset = size / 2 - height / 2;
 
-  splash.crop(width, height, xOffset, yOffset)
-    .write(target + name + '.png', function(err) {
-      if(err) {
-        console.log(err);
-        console.log("Quitting because of an error");
-      } else {
-        console.log("Wrote " + name);
-      }
-    });
+    splash
+        .crop(width, height, xOffset, yOffset)
+        .write(target + name + '.png', function(err) {
+            if (err) {
+                console.log(err);
+                console.log('Quitting because of an error');
+            } else {
+                console.log('Wrote ' + name);
+            }
+        });
 }
 
 // Run the code
 
-if(!fs.existsSync(__dirname + '/resources/icons')) {
-  fs.mkdirSync(__dirname + '/resources/icons');
+if (!fs.existsSync(__dirname + '/resources/icons')) {
+    fs.mkdirSync(__dirname + '/resources/icons');
 }
 
-if(!fs.existsSync(__dirname + '/resources/splashes')) {
-  fs.mkdirSync(__dirname + '/resources/splashes');
+if (!fs.existsSync(__dirname + '/resources/splashes')) {
+    fs.mkdirSync(__dirname + '/resources/splashes');
 }
 
 icons.forEach(function(icon) {
-  if (icon.name.indexOf('android') == 0) {
-    resize('resources/icon-android.png', 'resources/icons/', icon);
-  } else {
-    resize('resources/icon-ios.png', 'resources/icons/', icon);
-  }
+    if (icon.name.indexOf('android') == 0) {
+        resize('resources/icon-android.png', 'resources/icons/', icon);
+    } else {
+        resize('resources/icon-ios.png', 'resources/icons/', icon);
+    }
 });
 
 splashes.forEach(function(splash) {
-  if (splash.name.indexOf('android') == 0) {
-    crop('resources/splash-android.png', 'resources/splashes/', splash);
-  } else {
-    crop('resources/splash-ios.jpg', 'resources/splashes/', splash);
-  }
+    if (splash.name.indexOf('android') == 0) {
+        crop('resources/splash-android.png', 'resources/splashes/', splash);
+    } else {
+        crop('resources/splash-ios.png', 'resources/splashes/', splash);
+    }
 });
